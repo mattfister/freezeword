ba2c0f6b3fdf0623861e727f4b8cb91d94a8e98a
Matthew Fister <mfister@constantcontact.com>
2015-12-02 19:20:30 -0500

    Update docs

--- a_or_an.py
+++ a_or_an.py
@@ -1,4 +1,5 @@
 def a_or_an(word):
+    """Naively returns "a word" or "an word" depending on the word."""
     if len(word) > 0:
         if word[0] in 'aeiou':
             return 'an'
--- conceptnet_searcher.py
+++ conceptnet_searcher.py
@@ -1,5 +1,4 @@
 import json
-import sys
 import os
 
 # The number of json files that conceptnet is split over
@@ -7,7 +6,7 @@ num_parts = 50
 
 
 def string_hash(concept):
-    """Returns the hashed number that corresponds to the json file that the concept is in"""
+    """Returns the hashed number that corresponds to the json file that the concept is in."""
     sum = 0
     for i in range(len(concept)):
         sum += i*ord(concept[i])
@@ -16,7 +15,16 @@ def string_hash(concept):
 
 
 def get_concept_relations(concept):
-    """For the provided concept, return a list of relations that the concept has"""
+    """For the provided concept, return a list of relations that the concept has.
+
+    The returned relations is of the form of a list of lists. Each of the inner lists is a pair of
+    relations and concepts.
+
+    E.G.
+    get_concept_relations('avocado') returns
+    [['HasA', 'skin'], ['IsA', 'fruit'], ... ['Synonym', 'alligator_pear']]
+    """
+
     concept = concept.replace(' ', '_').lower()
     filename = 'conceptnet_reduced_en_' + str(string_hash(concept)) + '.json'
     with open(os.path.join(os.path.dirname(__file__), os.path.join('conceptnetreduced'), filename), 'r') as f:
